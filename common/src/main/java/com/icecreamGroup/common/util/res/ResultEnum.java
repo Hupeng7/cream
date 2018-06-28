53196bf00a3e947e5f090d2e8cc6a79e65e9f666
huangfeng <huangfeng@adinnet.cn>
2018-06-28 10:23:36 +0800

    常量池以及枚举类调整

--- common/src/main/java/com/icecreamGroup/common/util/res/ResultEnum.java
+++ common/src/main/java/com/icecreamGroup/common/util/res/ResultEnum.java
@@ -4,8 +4,7 @@ package com.icecreamGroup.common.util.res;
  * 统一错误返回
  * 自定义异常规范：模块 + 业务类型 + ERROR（log打印）
  * 对应的错误代码：1000是基数，每一个模块对应1000（针对与业务逻辑出错的错误代码，http错误代码不变且已定义）
- * 如基础服务类异常 ConfigServerConnectGitException 可以定义为10001
- * 如service层远程调用失败异常  RemoteCallException可以定义为11001
+ * 如service层远程调用失败异常  RemoteCallException可以定义为1999
  * 可以根据业务需要不断向后添加非http异常
  * http --------------------->100~1000
  * common ------------------->1000~2000
@@ -17,9 +16,9 @@ package com.icecreamGroup.common.util.res;
  * ...
  */
 public enum ResultEnum {
-    NOT_FOUND(404, "您的访问地址可能在火星上哦"),
+    NOT_FOUND(404, "不存在的映射路径"),
     REQUEST_TYPE_TO_METHOD_NOT_ALLOW(405, "请求类型错误"),
-    ERROR_UNKNOWN(500, "服务器正在与水星黑客展开殊死搏斗，请过几分钟再试"),
+    ERROR_UNKNOWN(500, "服务器跑路了呢.."),
     DATA_ERROR(1001,"数据转换错误"),
     MYSQL_OPERATION_FAILED(1002,"数据库sql操作失败"),
     INSERT_REPETITION(1003,"重复插入"),
