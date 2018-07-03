de26e5c14891d5a6747f5050dedf3482022dd5db
huangfeng <huangfeng@adinnet.cn>
2018-07-03 18:40:23 +0800

    ***Redis

---
+++ service-box/goods-service/src/main/java/com/icecream/good/controller/GoodController.java
@@ -0,0 +1,13 @@
+package com.icecream.good.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mr_h
+ * @version 1.0
+ * description:
+ * create by Mr_h on 2018/7/3 0003
+ */
+@RestController
+public class GoodController {
+}
