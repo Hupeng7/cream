b026a5a608a9c9b601d5ab80c8dcb85d68437897
huangfeng <huangfeng@adinnet.cn>
2018-09-11 11:14:54 +0800

    用户微服务注解@springBootApplication 优化为@springCloudApplication

--- service-box/user-service/src/main/java/com/icecream/user/UserServiceApplication.java
+++ service-box/user-service/src/main/java/com/icecream/user/UserServiceApplication.java
@@ -1,23 +1,13 @@
 package com.icecream.user;
 
 
-import com.icecream.user.utils.charge.RSAUtil;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.web.client.RestTemplate;
-import tk.mybatis.spring.annotation.MapperScan;
-
-import java.security.KeyPair;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 
 /**
  * @author  mr_h
@@ -25,9 +15,7 @@ import java.security.PublicKey;
  * 用户服务
  */
 @EnableFeignClients//开启feign客户端远程调用
-@EnableHystrix//开启断路器
-@EnableDiscoveryClient//开启可被服务中心发现
-@SpringBootApplication//springBoot项目，自动配置
+@SpringCloudApplication
 @EnableConfigurationProperties//开启自定义配置属性扫描
 @ComponentScan(basePackages = {"com.icecream.common","com.icecream.user"})//托管类扫描路径
 public class UserServiceApplication {
